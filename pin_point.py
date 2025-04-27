868b64519c194962aaca87f0e94276548bf1620a
Alliah <mahilumalliah002@gmail.com>
2025-04-27 10:36:02 +0800

    Define function for trying PINs and HTTP Post Form

--- pin_point.py
+++ pin_point.py
@@ -4,3 +4,17 @@ import time
 HOST = "127.0.0.1"
 PORT = 8888
 DELAY = 1.2 
+
+def try_pin(pin):
+    pin_str = f"{pin:03d}"  
+    data = f"magicNumber={pin_str}"
+
+    request = (
+        f"POST /verify HTTP/1.1\r\n"
+        f"Host: {HOST}:{PORT}\r\n"
+        f"Content-Type: application/x-www-form-urlencoded\r\n"
+        f"Content-Length: {len(data)}\r\n"
+        f"Connection: close\r\n"
+        f"\r\n"
+        f"{data}"
+    )
